d19295893a0fac14d4fb035d3be4c0a20f9bba0e
Dmitriy <dmkrch1@gmail.com>
2021-02-01 23:43:37 +0300

    some practice on copy constructor

---
+++ Introduction/CopyConstructor/copyConstructor.cpp
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <string.h>
+
+class Serialized
+{
+private:
+    static int NextSerialNumber;
+    int serialNumber;
+public:
+    /* copy constructor */
+    Serialized(const Serialized&);
+
+    /* default constructor */
+    Serialized();
+
+    /* function returns serial number */
+    int SerialNumber();
+};
+
+/* initializing static variable */
+int Serialized::NextSerialNumber = 0;
+
+
+Serialized::Serialized() : serialNumber(NextSerialNumber++) { }
+
+Serialized::Serialized(const Serialized&) : serialNumber(NextSerialNumber++) { }
+
+int Serialized::SerialNumber() { return serialNumber; }
+
+
+class Student : public Serialized
+{
+private:
+    char* name;
+    int age;
+public:
+    Student(int age, const char* name)
+    {
+        this->age = age;
+        this->name = strdup(name);
+    }
+
+    void PrintID() { printf("%d ", this->SerialNumber()); }
+};
+
+int main()
+{
+    Student st1(25, "Dmitriy");
+    Student st2(18, "Anton");
+    Student st3(19, "Sasha");
+
+    st1.PrintID();
+    st2.PrintID();
+    st3.PrintID();
+    
+    return 0;
+}
