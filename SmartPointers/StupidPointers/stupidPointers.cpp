e75bf9d4f5390e710edcd1db124e89d30f7129b9
Dmitriy <dmkrch1@gmail.com>
2021-02-11 10:51:24 +0300

    stupid pointers fixed to smart

--- SmartPointers/StupidPointers/stupidPointers.cpp
+++ SmartPointers/StupidPointers/stupidPointers.cpp
@@ -20,6 +20,8 @@ public:
     PFoo() : foo(nullptr) {}
     PFoo(Foo* f) : foo(f) {}
     operator Foo*() { return foo; }
+    Foo* operator->() { return foo; }
+
     PFoo operator+(ptrdiff_t offset) { return PFoo(foo + offset); }
     PFoo operator+=(ptrdiff_t offset) { foo += offset; return *this; }
     ptrdiff_t operator-(PFoo pf) { return foo - pf.foo; }
@@ -27,9 +29,15 @@ public:
     // and so on, for all the operations with pointers
 };
 
+void Func(Foo* pointer)
+{
+    delete pointer;
+}
+
 int main()
 {
-    PFoo pf;
+    PFoo pf; 
+
     Foo* foo = new Foo(5);
 
     printf("%d\n", foo->Value());
@@ -41,8 +49,19 @@ int main()
 
     //printf("%d", pf->Value()); // we get an error
 
-    printf("%d\n", ((Foo*)pf)->Value()); // ok
+    printf("%d\n", ((Foo*)pf)->Value()); // fine. But looks awful
 
     // output: 5 5
+
+
+
+    /* so actually operator Foo*() allows passing PFoo to functions,
+       that accepts Foo*. Moreover, we can do all the ariphmetic
+       operations. But they are 'stupid', because operator
+       -> doesnt work the way they should. Now we fix it adding
+       an overload operator -> */
+    // Foo* operator->() { return foo; }
+    printf("%d\n", pf->Value()); // now fine
+
     return 1;
 }
